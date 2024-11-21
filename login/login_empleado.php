62f8327784763fff0f3409b9d5fdb3b77477bb7b
kevinezequielnavarro <130410767+kevinezequielnavarro@users.noreply.github.com>
2024-11-21 15:35:58 -0300

    Update login_empleado.php switch para tipo_empl

--- login/login_empleado.php
+++ login/login_empleado.php
@@ -10,43 +10,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $dni = $_POST['dni'];
     $password = $_POST['password'];
     
-    var_dump($dni, $password); 
-    
-    $sql = "SELECT pe.id_empleado, pe.password_hash 
+    $sql = "SELECT pe.id_empleado, pe.password_hash, e.tipo_empleado, e.nombre, e.apellido, t.id
             FROM password_empleados pe
-            JOIN empleado e ON pe.id_empleado = e.id_empleado  
+            JOIN empleado e ON pe.id_empleado = e.id_empleado 
+            JOIN tipo_empleado t ON t.id = e.tipo_empleado 
             WHERE e.dni = :dni;";
 
-    echo $sql;
     try {       
         $stmt = $pdo->prepare($sql);
-        
         $stmt->bindParam(':dni', $dni);
-        
         $stmt->execute();
-        echo "0";  
 
         if ($stmt->rowCount() > 0) {
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             $storedHash = $row['password_hash'];
-            echo "1";  
+            $id_tipoempleado = $row['id'];
+            $user = $user = $row['nombre'] . ' ' . $row['apellido']; 
+
+
             if (password_verify($password, $storedHash)) {                
                 $_SESSION['empleado_id'] = $row['id_empleado'];
                 $_SESSION['dni'] = $dni;
-                echo "2";  
-                header('Location: ../index_empleados/index.php');  
+                $_SESSION['user'] = $user;
+                $_SESSION['id_empleado'] = $id_empleado;
+
+                switch ($id_tipoempleado) {
+                    case 1:
+                        $_SESSION['id_tipoempleado'] = 1;
+                        header('Location: ../index_empleados/index_radiologo.php');
+                        break;
+
+                    case 2:
+                        $_SESSION['id_tipoempleado'] = 2;
+                        header('Location: ../index_empleados/index_recepcionista.php');
+                        break;
+                        
+                    default:
+                        $_SESSION['error'] = "Tipo de empleado no reconocido.";
+                        break;
+                }
                 exit;
             } else {
-                $error = "Contraseña incorrecta.";
-                echo "3";  
+                $_SESSION['error'] = "Contraseña incorrecta.";
             }
         } else {
-            $error = "Empleado no encontrado.";
-            echo "4";  
+            $_SESSION['error'] = "Empleado no encontrado.";
         }
     } catch (PDOException $e) {
-        $error = "Error al conectar con la base de datos: " . $e->getMessage();
+        $_SESSION['error'] = "Error al conectar con la base de datos: " . $e->getMessage();
     }
 }
-
 ?>
