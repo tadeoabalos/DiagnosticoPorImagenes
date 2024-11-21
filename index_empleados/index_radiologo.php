5ff778b2e9a8c1b44b24b22838529c3b456accf4
kevinezequielnavarro <130410767+kevinezequielnavarro@users.noreply.github.com>
2024-11-21 15:16:38 -0300

    Update index_radiologo.php

--- index_empleados/index_radiologo.php
+++ index_empleados/index_radiologo.php
@@ -1,15 +1,16 @@
 <?php
 session_start();
-?>
+if (!isset($_SESSION['empleado_id'])) {
+    header('Location: ../index/index.php');  // Redirigir si no está autenticado
+    exit;
+}
 
-<!DOCTYPE html>
-<html lang="es">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-<?php
 $pageTitle = 'Radiologo';
 
 include '../utils/header.php';
+include '../conexion.php';
 ?>
+
 <style>
     html,
     body {
@@ -25,7 +26,36 @@ include '../utils/header.php';
         flex: 1;
     }
 </style>
+<!DOCTYPE html>
+<html lang="es">
 
+<script>
+    $(document).on('click', '#view_turno', function(event) {
+        var id_paciente_turno = $(this).data('id');
+        var action = 'radiologo';
+
+        $.ajax({
+            url: "modal_info_pacientes.php",
+            type: 'POST',
+            data: {
+                paciente: id_paciente_turno,
+                action : action
+            },
+            cache: false,
+            dataType: 'html',
+            success: function(data) {
+                $('#modal_turno').remove();
+                var html = data;
+                $('body').append(html);
+                $('#modal_turno').modal('show');
+            },
+            error: function(xhr, status, error) {
+                console.error("Error al cargar el modal:", error);
+                alert("Hubo un problema al cargar los datos. Por favor, intenta nuevamente.");
+            }
+        });
+    });
+</script>
 <div class="container py-5">
     <div class="table-responsive" id="impositivo-content">
 
@@ -35,56 +65,41 @@ include '../utils/header.php';
                 <tr>
                     <th></th>
                     <th style="vertical-align: middle;" class="text-center"><strong>Paciente</strong></th>
-                    <th style="vertical-align: middle;" class="text-center"><strong>Turno</strong></th>
                     <th style="vertical-align: middle;" class="text-center"><strong>Especialidad</strong></th>
+                    <th style="vertical-align: middle;" class="text-center"><strong>Turno</strong></th>
                     <th></th>
                 </tr>
             </thead>
             <tbody id="tbody_pacientes">
                 <?php
-                // $stmt = $pdo->prepare("SELECT ..."); //
-                // $stmt->execute();
+                 $stmt = $pdo->prepare("SELECT tp.fecha, tp.hora, p.*, e.nombre AS especialidad 
+                 FROM turnos_pacientes tp 
+                 JOIN paciente p ON p.id_paciente = tp.id_paciente 
+                 JOIN especializacion e ON e.id_especializacion = tp.id_especializacion 
+                 WHERE 1=1 ORDER BY fecha");
+                $stmt->execute();
 
-                // if ($stmt->rowCount() > 0) {
-                //     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                //         echo "<tr>";
-                //         echo "<td class='text-center'>";
-                //         echo "<button class='btn btn-sm btn-outline-primary' id='view_turno' title='Ver Turno' data-bs-toggle='modal' data-bs-target='#modal_turno' data-name='" . htmlspecialchars($row['usuario_nombre']) . "'><i class='fas fa-eye'></i></button>";
-                //         echo "</td>";
-                //         echo "<td>" . htmlspecialchars($row['usuario_nombre']) . "</td>";
-                //         echo "<td>" . htmlspecialchars($row['fecha_turno']) . "</td>";
-                //         echo "<td>" . htmlspecialchars($row['especialidad']) . "</td>";
-                //         echo <td style="vertical-align: middle;" class="text-center">
-                //         echo       <button class="btn btn-sm btn-success" id="add_button" title="Subir datos" data-bs-toggle="modal" data-bs-target="#modal_add_rad">
-                //         echo           <i class="fas fa-upload"></i> Subir
-                //         echo      </button>
-                //         echo </td>
-                //         echo "</tr>";
-                //     }
-                // } else {
-                echo "<tr><td colspan='5' class='text-center'>No hay turnos disponibles</td></tr>";
-                //}
+                if ($stmt->rowCount() > 0) {
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<tr>";
+                        echo "<td class='text-center'><a class=\"btn btn-sm btn-outline-primary\" id=\"view_turno\" data-id=\"" . $row['id_paciente'] . "\">
+                            <i class=\"fas fa-eye\"></i></a></td>";
+                        echo "<td style='vertical-align: middle;' class='text-center'>" . htmlspecialchars($row['nombre'] . ' ' . $row['apellido']) . "</td>";
+                        echo "<td style='vertical-align: middle;' class='text-center'>" . htmlspecialchars($row['especialidad']) . "</td>";
+                        echo "<td style='vertical-align: middle;' class='text-center'>" . htmlspecialchars($row['hora'] . ' ' . $row['fecha']) . "</td>";
+                        echo "<td style='vertical-align: middle;' class='text-center'>";
+                        echo "<button class='btn btn-sm btn-success' id='add_button' title='Subir datos' data-bs-toggle='modal' data-bs-target='#modal_add_rad'>";
+                        echo "<i class='fas fa-upload'></i> Subir";
+                        echo "</button>";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='5' class='text-center'>No hay turnos disponibles</td></tr>";
+                }
                 ?>
             </tbody>
         </table>
-    </div>
-</div>
-
-<div class="modal fade" id="modal_turno" tabindex="-1" aria-labelledby="modal_turnoLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title w-100 text-center" id="modal-view_turno-label">
-                    Informaci&oacute;n del paciente:
-                    <!-- <?php //echo htmlspecialchars($idTurno); 
-                            ?> -->
-                </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-
-            </div>
-        </div>
     </div>
 </div>
 
@@ -107,12 +122,13 @@ include '../utils/header.php';
                     </div>
 
                     <div class="mb-3">
-                        <label for="description" class="form-label"><strong>Descripción</strong></label>
-                        <input type="text" class="form-control" id="description" name="description" placeholder="Ingrese una descripción" required>
+                        <label for="nota" class="form-label"><strong>Nota:</strong></label>
+                        <input type="text" class="form-control" id="nota" name="nota" placeholder="Ingrese una nota" required>
                     </div>
 
                     <div class="d-flex justify-content-end">
                         <button type="button" class="btn btn-primary" id="save_rad">Guardar</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                     </div>
                 </form>
             </div>
